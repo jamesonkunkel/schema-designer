d4d73add50f0fc1f333ed910a10af048990777c7
jamesonkunkel <jameson.kunkel@gmail.com>
2023-12-11 14:20:27 -0500

    added help tooltip

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 
+//import assets
+import HelpIcon from "../../assets/HelpIcon";
+
 function Header() {
   return (
     <div className="navbar bg-base-200">
@@ -7,6 +10,12 @@ function Header() {
         <Link to="/" className="btn btn-ghost text-xl">
           Schema Drafter
         </Link>
+      </div>
+      <div
+        className="flex-none tooltip tooltip-left"
+        data-tip="Drag from the bottom handle of the Root node (or any object node) to create a new property."
+      >
+        <HelpIcon />
       </div>
     </div>
   );
